2cdf9f55743e1bb77d1605b9a51117dd3133d874
mahmoudnaserddine <mahmoudnaserddine81@gmail.com>
2023-09-08 14:58:48 +0300

    updated

--- assignment3.py
+++ assignment3.py
@@ -91,56 +91,175 @@
 
 
 
-class Stack:
-    def __init__(self):
-        self.elements = []
-
-    def push(self, item):
-        self.elements.append(item)
-
-    def pop(self):
-        if len(self.elements) == 0:
-            return None
-        return self.elements.pop()
-
+# class Stack:
+#     def __init__(self):
+#         self.elements = []
 #
-def is_valid_expression(expression):
-    # Create a mapp of  parentheses
-    parentheses_map = {')': '(', '}': '{', ']': '['}
-
-    # Create an instance of the custom Stack class
-    stack = Stack()
-
-    # Iterate through each character in the expression
-    for char in expression:
-        # If the character is an opening parenthesis or curly brace, push it onto the stack
-        if char in '({[':
-            stack.push(char)
-        # If the character is a closing parenthesis or curly brace, check for a matching opening character
-        elif char in ')}]':
-            # If the stack is empty, there's no matching opening character, so return False
-            if not stack.elements:
-                return False
-            # Check if the last character in the stack matches the expected opening character
-            if stack.pop() == parentheses_map.get(char):
-                continue  # Match found, continue checking the next character
-            else:
-                return False
-
-    # After iterating through the entire expression, check if the stack is empty (all characters were balanced)
-    return not stack.elements
-
-# Get user input for the expression to be checked
-user_input = input("Enter an expression: ")
-
-# Check if the entered expression contains valid combinations of parentheses and curly braces using the is_valid_expression function
-if is_valid_expression(user_input):
-    print("True")
-else:
-    print("False")
+#     def push(self, item):
+#         self.elements.append(item)
+#
+#     def pop(self):
+#         if len(self.elements) == 0:
+#             return None
+#         return self.elements.pop()
+#
+# #
+# def is_valid_expression(expression):
+#     # Create a mapp of  parentheses
+#     parentheses_map = {')': '(', '}': '{', ']': '['}
+#
+#     # Create an instance of the custom Stack class
+#     stack = Stack()
+#
+#     # Iterate through each character in the expression
+#     for char in expression:
+#         # If the character is an opening parenthesis or curly brace, push it onto the stack
+#         if char in '({[':
+#             stack.push(char)
+#         # If the character is a closing parenthesis or curly brace, check for a matching opening character
+#         elif char in ')}]':
+#             # If the stack is empty, there's no matching opening character, so return False
+#             if not stack.elements:
+#                 return False
+#             # Check if the last character in the stack matches the expected opening character
+#             if stack.pop() == parentheses_map.get(char):
+#                 continue  # Match found, continue checking the next character
+#             else:
+#                 return False
+#
+#     # After iterating through the entire expression, check if the stack is empty (all characters were balanced)
+#     return not stack.elements
+#
+# # Get user input for the expression to be checked
+# user_input = input("Enter an expression: ")
+#
+# # Check if the entered expression contains valid combinations of parentheses and curly braces using the is_valid_expression function
+# if is_valid_expression(user_input):
+#     print("True")
+# else:
+#     print("False")
 #--------------------------------------------------------------------------------------------------
 
+#question 3:
 
+
+
+
+
+
+
+
+class Node:
+    def __init__(self, info, next=None):
+        self.info = info
+        self.next = next
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.size = 0  # number of nodes in my LL
+
+    def addToTail(self, info):
+        n = Node(info)
+        if self.size == 0:
+            self.head = n
+            self.tail = n
+            self.size += 1
+        else:
+            self.tail.next = n
+            self.tail = n
+            self.size += 1
+
+    def deleteHead(self):
+        if self.size == 0:
+            return None
+        if self.size == 1:
+            temp = self.head.info
+            self.head = None
+            self.tail = None
+            self.size = 0
+            return temp
+
+        temp = self.head.info
+        self.head = self.head.next
+        self.size -= 1
+        return temp
+
+class Queue:
+    def __init__(self):
+        self.elements = LinkedList()
+
+    def enqueue(self, item):
+        self.elements.addToTail(item)
+
+    def dequeue(self):
+        return self.elements.deleteHead()
+
+    def size(self):
+        return self.elements.size
+
+    def isEmpty(self):
+        return self.elements.size == 0
+
+    def front(self):
+        if self.isEmpty():
+            return None
+        return self.elements.head.info
+
+# Car class to represent car information
+class Car:
+    def __init__(self, make, color, plate_number):
+        self.make = make
+        self.color = color
+        self.plate_number = plate_number
+
+
+
+
+# Create a queue to manage cars waiting for a car wash
+car_queue = Queue()
+
+# Create an infinite loop to display the car wash menu until the user quits
+while True:
+    # Display the car wash menu options
+    print("Car Wash Menu:")
+    print("1. Insert a car to the queue")
+    print("2. To remove the car from the queue")
+    print("3. Quit")
+
+    # Prompt the user for their choice
+    choice = input("Enter your choice: ")
+
+
+    if choice == '1':
+        # Prompt the user for car details and create a car object
+        make = input("Enter car make: ")
+        color = input("Enter car color: ")
+        plate_number = int(input("Enter car plate number: "))
+        car = Car(make, color, plate_number)
+
+        # Add the car to the queue and provide feedback
+        car_queue.enqueue(car)
+        print(f"{car.make} {car.color} ({car.plate_number}) has been added to the queue.")
+
+    elif choice == '2':
+        # Check if the queue is empty before attempting to dequeue
+        if car_queue.isEmpty():
+            print("The car wash queue is empty.")
+        else:
+            # Dequeue the next car and display a message
+            car = car_queue.dequeue()
+            print(f"Car washed: {car.make} {car.color} ({car.plate_number})")
+
+    elif choice == '3':
+        # Exit the loop if the user chooses to quit
+        break
+    else:
+        # Handle invalid choices
+        print("Invalid choice. Please select a valid option.")
+
+#------------------------------------------------------------------------------------
 
 
 
